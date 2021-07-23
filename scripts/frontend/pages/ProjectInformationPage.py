595597c06e21f0a598cc9ad6b33fd959f7b9a969
mlapshin <mlapshin@uwaterloo.ca>
2021-07-22 23:20:24 -0400

    Completely finished the Project Information page.

--- scripts/frontend/pages/ProjectInformationPage.py
+++ scripts/frontend/pages/ProjectInformationPage.py
@@ -10,6 +10,7 @@ class Frame(GenericPage.NavigationFrame):
     TITLE = "Project Information"
     SUB_TITLE_FONT_SIZE = 12
     TEXT_FONT_SIZE = 9
+    CREDITS_FONT_SIZE = 10
 
     def __init__(self, root, base_frame=None):
         GenericPage.NavigationFrame.__init__(self, root=root, base_frame=base_frame,
@@ -19,9 +20,28 @@ class Frame(GenericPage.NavigationFrame):
         # Create settings title
         self.settings_title = CustomLabels.TitleLabel(root=self, column=0, row=0, text="Project Information")
 
+        # Credits
+        self.credits = InformationBlock.Frame(self,
+                                              column=0, row=1,
+                                              num_columns=2, num_rows=1,
+                                              frame_colour=Constants.COLOUR_ALPHA,
+                                              label_colour=Constants.COLOUR_BRAVO)
+        self.credits.add_info(column=0, row=0, text="Author: Michael Lapshin\n\n"
+                                                    "Project: On-going since November 2020")
+
+        self.credits.add_info(column=1, row=0,
+                              text="--- Ultimate Goal ---\n\n"
+                                   "To create a hardware-software interface to accurately translate hand-produced\n"
+                                   " forces into realistic, believable hand movements in a virtual environment.")
+        self.credits.set_column_weight(1, weight=3)
+
+        # Credits fonts
+        self.credits.set_font(column=0, row=0, font_size=Frame.CREDITS_FONT_SIZE)
+        self.credits.set_font(column=1, row=0, font_size=Frame.CREDITS_FONT_SIZE)
+
         # Creates the info block
         self.info_block = InformationBlock.Frame(self,
-                                                 column=0, row=1,
+                                                 column=0, row=2,
                                                  num_columns=3, num_rows=6,
                                                  frame_colour=Constants.COLOUR_ALPHA,
                                                  label_colour=Constants.COLOUR_BRAVO)
@@ -53,10 +73,10 @@ class Frame(GenericPage.NavigationFrame):
                                                        "Hardware + electronics:\n"
                                                        " - Arduino, amplifiers\n"
                                                        " - Load-cell sensors")
-        self.info_block.add_info(column=1, row=3, text="Unity\n"
-                                                       "TCP connection handling\n"
+        self.info_block.add_info(column=1, row=3, text="TCP connection handling:\n"
                                                        " - C# client, Python client"
-                                                       " - Python server")
+                                                       " - Python server\n\n"
+                                                       "Unity")
         self.info_block.add_info(column=1, row=4, text="REST API Server connection handling\n"
                                                        "Database manipulation\n"
                                                        "Sub-process running")
@@ -77,17 +97,13 @@ class Frame(GenericPage.NavigationFrame):
                                                        " - Tensorflow, sequential models\n"
                                                        " - Multithreading\n"
                                                        " - Matplotlib library")
-        self.info_block.add_info(column=2, row=2, text="Programming Languages: Arduino (C++), Python 3.8\n"
-                                                       "\n"
-                                                       "Key Skills:\n"
-                                                       " - Object-oriented programming\n"
-                                                       " - Communication port interfaces\n"
-                                                       " - Multithreading\n"
-                                                       "\n"
-                                                       "Key APIs:\n"
-                                                       "- Matplotlib library\n"
-                                                       "- OpenCV (camera API)\n"
-                                                       "- Mediapipe (hand tracking API)")
+        self.info_block.add_info(column=2, row=2,
+                                 text="Programming Languages: Arduino (C++), Python 3.8\n"
+                                      "\n"
+                                      "Key Skills:                                            Key APIs:\n"
+                                      " - Object-oriented programming                  - Matplotlib library\n"
+                                      " - Communication port interfaces                - OpenCV (camera API)\n"
+                                      " - Multithreading                                        - Mediapipe (hand tracking API)")
         self.info_block.add_info(column=2, row=3, text="Programming Languages: C# .NET, Python 3.8\n"
                                                        "\n"
                                                        "Key skills:                               Key APIs:\n"
@@ -104,7 +120,7 @@ class Frame(GenericPage.NavigationFrame):
                                                        "Key skills and APIs:\n"
                                                        " - Tkinter (GUI) library\n"
                                                        " - Python flask, REST API\n"
-                                                       " - Object-oriented programming"
+                                                       " - Object-oriented programming\n"
                                                        " - Multithreading")
         for i in range(1, 6):
             self.info_block.set_justify(column=2, row=i, justify=tkinter.LEFT)
@@ -115,6 +131,10 @@ class Frame(GenericPage.NavigationFrame):
         super().update_colour()
 
         self.settings_title.update_colour()
+
+        self.credits.set_frame_colour(Constants.COLOUR_ALPHA)
+        self.credits.set_label_colour(Constants.COLOUR_BRAVO)
+        self.credits.update_colour()
 
         self.info_block.set_frame_colour(Constants.COLOUR_ALPHA)
         self.info_block.set_label_colour(Constants.COLOUR_BRAVO)
