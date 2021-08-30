4b6cff479084f4aa7a591ebc985c2f5e16e91146
mlapshin <mlapshin@uwaterloo.ca>
2021-08-29 22:34:04 -0400

    Added a server console reader. Not working at the moment; deadlocking.

--- scripts/backend/connection/ServerApp.py
+++ scripts/backend/connection/ServerApp.py
@@ -6,11 +6,14 @@
 
 # Import common libraries
 import os
+import threading
 import time
 
 import flask
 
 # Import REST API scripts
+import requests
+
 from API_Helper import package
 from AccountAPI import account_api
 from FileTransferAPI import file_transfer_api
@@ -19,20 +22,89 @@ from DataFetchingAPI import data_fetching_api
 from DataUpdatingAPI import data_updating_api
 
 # Import miscellaneous scripts
-from scripts import Log, Warnings
+from scripts import Log, Warnings, General
 from scripts.backend.database import Database
-
-# Questionnaire for the server setup
-
 
 # Database
 from scripts.backend.logic import Worker
 
+# Questionnaire for the server setup
+
+
+HOST = "127.0.0.1"
+PORT = 5000
 start_server = Database.connect("server_database.db")
 Database.create_all_new_tables(replace=False)
 workers = []
 
+
+class ConsoleReader(threading.Thread):
+    def __init__(self, stop_processes_command):
+        threading.Thread.__init__(self)
+        self._stop_processes_command = stop_processes_command
+        self._running = False
+        self.daemon = True
+        self._stopped = True
+
+    def run(self):
+        self._stopped = False
+        while self._running is True:
+            inp = input()
+            if inp.lower() == "stop" or inp.lower() == "shutdown":
+                # requests.get(HOST + ":" + str(PORT) + "/shutdown"
+                print("[ServerApp] Stopping the processes.")
+                self._stop_processes_command()
+                print("[ServerApp] All processes have been stopped.")
+                print("[ServerApp] You may now stop the (flask) server.")
+            else:
+                print("[ServerApp] Did not recognize the command.")
+            time.sleep(3)
+        self._stopped = True
+        Log.info("The  console reader thread has stopped.")
+
+    def start(self):
+        Log.info("Starting the console reader thread...")
+        self._running = True
+        super().start()
+
+    def stop(self):
+        Log.info("Stopping the console reader thread...")
+        self._running = False
+
+    """
+        Getters
+    """
+
+    def is_running(self):
+        return self._running
+
+    def is_stopped(self):
+        return self._stopped
+
+
+def stop_processes():
+    # Stops the console reader
+    if console_reader is not None:
+        console_reader.stop()
+        while console_reader.is_stopped() is False:
+            time.sleep(1)
+
+    # Stops the workers
+    for w in workers:
+        if w is not None:
+            w.stop()
+
+    # Waits until the workers are done their tasks
+    for w in workers:
+        while w.is_stopped() is False:
+            time.sleep(1)
+
+    # Disconnects the database
+    Database.disconnect()
+
+
 if start_server:
+
     # Flask application + configurations
     Log.info("Starting the server...")
     server_app = flask.Flask(__name__)
@@ -42,6 +114,10 @@ if start_server:
     server_app.register_blueprint(data_processing_api, url_prefix="/process")
     server_app.register_blueprint(data_fetching_api, url_prefix="/fetch")
     server_app.register_blueprint(data_updating_api, url_prefix="/update")
+
+    # Create console reader
+    console_reader = ConsoleReader(stop_processes)
+    console_reader.start()
 
     # Creates workers
     Worker.dataset_worker = Worker.Worker()
@@ -60,23 +136,12 @@ if start_server:
         w.start()
 
     if __name__ == "__main__":
-        server_app.run(host="127.0.0.1", port=5000, threaded=True)
+        server_app.run(host=HOST, port=PORT, threaded=True)
 
 
 @server_app.route('/shutdown')
 def shutdown():
-    # Stops the workers
-    for w in workers:
-        if w is not None:
-            w.stop()
-
-    # Waits until the workers are done their tasks
-    for w in workers:
-        while w.is_stopped() is False:
-            time.sleep(1)
-
-    # Disconnects the database
-    Database.disconnect()
+    stop_processes()
 
     # Shutting down the flask server
     shutdown_func = flask.request.environ.get('werkzeug.server.shutdown')
