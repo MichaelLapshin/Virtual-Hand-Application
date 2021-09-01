46119cfe5d388c92d67578a6ac82e2b463759dd6
mlapshin <mlapshin@uwaterloo.ca>
2021-09-01 16:42:46 -0400

    Added start and end time for worker tasks. Added clear complete queue method.

--- scripts/logic/Worker.py
+++ scripts/logic/Worker.py
@@ -3,7 +3,7 @@ import threading
 # Workers
 import time
 
-from scripts import Log
+from scripts import Log, General
 
 dataset_worker = None
 dataset_image_worker = None
@@ -22,6 +22,8 @@ class Worker(threading.Thread):
         self.daemon = True
         self._queue = []
         self._complete = []
+        self._start_time = []
+        self._end_time = []
         self._stopped = True
         self._sleep_delay = sleep_delay
 
@@ -33,7 +35,11 @@ class Worker(threading.Thread):
         while self._running is True:
             while len(self._queue) > 0:
                 # TODO, change 'while' to 'if' once you get the worker tasks to be stored within the database
+                self._start_time.append(General.get_current_slashed_date())
                 self._queue[0].perform_task()
+                self._end_time.append(General.get_current_slashed_date())
+
+                # Transfers the task to the queue complete
                 self._complete.append(self._queue[0])
                 self._queue.pop(0)
             time.sleep(self._sleep_delay)
@@ -49,6 +55,12 @@ class Worker(threading.Thread):
     def stop(self):
         Log.info("Stopping the worker thread...")
         self._running = False
+
+    def clear_complete_queue(self):
+        for i in range(0, len(self._complete)):
+            self._start_time.pop(0)
+            self._end_time.pop(0)
+            self._complete.pop(0)
 
     """
         Getters
